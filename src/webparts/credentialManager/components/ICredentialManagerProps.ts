f431e15ca33ed6f5881f02909f4531a60aa19421
BaigFarhan <39454407+BaigFarhan@users.noreply.github.com>
2018-05-23 04:06:10 +0400

    Update ICredentialManagerProps.ts

--- src/webparts/credentialManager/components/ICredentialManagerProps.ts
+++ src/webparts/credentialManager/components/ICredentialManagerProps.ts
@@ -1,7 +1,12 @@
-export interface ICredentialManagerProps {
+import { SPHttpClient } from '@microsoft/sp-http';
+export interface ICredentailManagerProps {
   description: string;
-  ProjectName:string;
+  isModalOpen:boolean;
+ ProjectName:string;
   UserId:string;
   Password:string;
-
+  ShowModal: boolean;
+  Key:string,
+  spHttpClient: SPHttpClient;
+  SitrUrl:string;
 }
