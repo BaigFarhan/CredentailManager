7246153855c561fb092b30bfe256a11b6a0c3b13
BaigFarhan <39454407+BaigFarhan@users.noreply.github.com>
2018-05-23 04:05:17 +0400

    Update CredentialManager.tsx

--- src/webparts/credentialManager/components/CredentialManager.tsx
+++ src/webparts/credentialManager/components/CredentialManager.tsx
@@ -1,28 +1,72 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import  {Component} from 'react'
-import styles from './CredentialManager.module.scss';
-import { ICredentialManagerProps } from './ICredentialManagerProps';
+import styles from './CredentailManager.module.scss';
+import { ICredentailManagerProps } from './ICredentailManagerProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import { FormControl,GridForm,Form, FormGroup,Col,Fieldset, Row, Field } from 'react-gridforms'
+import { SPComponentLoader } from '@microsoft/sp-loader';
+import { Button, Modal } from 'react-bootstrap';
+import { FormControl, GridForm, Form, FormGroup, Col, Fieldset, Row, Field } from 'react-gridforms'
 import { PropertyPaneCheckbox } from '@microsoft/sp-webpart-base';
- 
-export default class CredentialManager extends React.Component<ICredentialManagerProps, {}> {
-  
-  public state: ICredentialManagerProps;
+import Dialog from 'react-bootstrap-dialog';
+import * as jquery from 'jquery';
+import * as pnp from 'sp-pnp-js';
+import {
+  SPHttpClient,
+  SPHttpClientBatch,
+  SPHttpClientResponse, SPHttpClientConfiguration
+} from '@microsoft/sp-http';
+
+var CryptoJS = require("crypto-js");
+export default class CredentailManager extends React.Component<ICredentailManagerProps, {}> {
+
+
+  protected onInit(): Promise<void> {
+    return new Promise<void>((resolve: () => void, reject: (error?: any) => void): void => {
+      pnp.setup({
+        sp: {
+          headers: {
+            "Accept": "application/json; odata=nometadata"
+          }
+        }
+      });
+      resolve();
+    });
+  }
+
+  public state: ICredentailManagerProps;
+
   constructor(props, context) {
+
     super(props);
-    this.state={
+    this.state = {
+      isModalOpen: false,
       description: "",
       ProjectName: "",
       UserId: "",
       Password: "",
-    };
+      Key: "",
+      ShowModal: false,
+      spHttpClient: this.props.spHttpClient,
+      SitrUrl: 'https://mirzaa.sharepoint.com/sites/dev',
+    }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css');
+    }
+
+  handleClose(e) {
+
+    this.setState({ isModalOpen: false });
+    return false;
   }
 
-  
+  handleShow(e) {
+
+    this.setState({ isModalOpen: true });
+    e.preventDefault();
+    return false;
+  }
+
   public OnChangeProject(event: any): void {
     this.setState({ ProjectName: event.target.value });
   }
@@ -39,67 +83,136 @@ export default class CredentialManager extends React.Component<ICredentialManage
     this.setState({ description: event.target.value });
   }
 
-  handleClick(e) {
-    e.preventDefault();
-     alert(this.state.ProjectName + this.state.Password + this.state.UserId + this.state.description);
 
-     
-     return false;
-     
+  public OnChangeKey(event: any): void {
+    this.setState({ Key: event.target.value });
   }
+
+
+
+  componentDidMount() {
+    //this.GetUSerDetails();
+  }
+
+  private GetUSerDetails() {
+
+    var reactHandler = this;
+    debugger;
+    var reqUrl = reactHandler.state.SitrUrl + "/_api/lists/getbytitle('Cred-Manager')/items";
+    //farhan
+
+    jquery.ajax({
+      url: reqUrl, type: "GET",
+      headers:
+        {
+          "accept": "application/json;odata=verbose"
+        }
+    }).then((response) => {
+      console.log(response.d);
+      debugger;
+
+    });
+
+  }
+  SaveData(e) {
 
   
-  public render(): React.ReactElement<ICredentialManagerProps> {
-    
-    return (
-      <div className={styles.credentialManager }>
-       <h2>Sign up</h2>
-       <br/>
-      <GridForm>
-                <Row>
-                  <Field span={1}>
-                    <label>App Name  :</label>
-                    <input type="text"  value={this.state.ProjectName} onChange={this.OnChangeProject.bind(this)} />
-                </Field>
-                  
-                </Row>
-                <Row>
-                  <Field span={4}>
-                    <label>User Name  :</label>                    
-                    <input type="text"  value={this.state.UserId} onChange={this.OnChangeUserID.bind(this)} />
-                  </Field>
-                 
-                </Row>
-                <Row>
-                  <Field span={4}>
-                    <label>Password</label>                    
-                    <input type="Password"  value={this.state.Password} onChange={this.OnChangePassword.bind(this)} />
-                  </Field>
-                 
-                </Row>
-                <Row>
-                  <Field span={4}>
-                    <label>Description</label>
-                    <input type="text"  value={this.state.description} onChange={this.OnChangeDescription.bind(this)} />
-                  </Field>
-                 
-                </Row>
-                <Row>
-                  <Field span={4}>
-                  <input type="submit"  onClick={this.handleClick} />                 
-                  
-                  </Field>
-                 
-                </Row>
+    var AppName = this.state.ProjectName;
+    var UserId = this.state.UserId;
+    var PasswordKey = this.state.Password + this.state.Key;
+    var Description = this.state.description;
+    var ciphertext = CryptoJS.AES.encrypt(PasswordKey, 'composedownkey');
+    //console.log("encrypted text", ciphertext.toString());
 
+    // var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
+    // var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+    // console.log("decrypted text", plaintext);
+
+    pnp.sp.web.lists.getByTitle('Cred-Manager').items.add({
+      'Title': "Some title",
+      'AppName': AppName,
+      'UserName1': UserId,
+      'Password': this.state.Password,
+      'CMDescription': Description,
+      'Key': ciphertext.toString()
+
+    }).then((result): void => {
+
+      this.state.ProjectName = ''
+      this.state.UserId = '';
+      this.state.Password = '';
+      this.state.Key = '';
+      var Description = this.state.description = '';
+      this.setState({ isModalOpen: false });
+      alert('sucessfully added');
+
+    }, (error: any): void => {
+      alert(error);
+    });
+
+  }
+
+  public render(): React.ReactElement<ICredentailManagerProps> {
+    return (
+      <div className={styles.credentailManager}>
+        <div className={"well well-sm"}><h3> <span className={"label label-default"}>Sign Up</span></h3></div>
+
+        <Modal show={this.state.isModalOpen} onHide={this.handleClose}>
+          <Modal.Header >
+            <Modal.Title>Provide Password Key</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <GridForm>
+              <Row>
+                <Field span={1}>
+                  <label>Enter The Password Key :</label>
+                  <input type="password" value={this.state.Key} onChange={this.OnChangeKey.bind(this)} />
+                </Field>
+              </Row>
             </GridForm>
-{this.state.UserId}
-{this.state.Password}
-{this.state.ProjectName}
-     
-      </div> 
-    
-   )
+            <br />
+            <button className={"btn btn-success"} onClick={this.SaveData.bind(this)}>Save</button> &nbsp;
+            <button className={'btn btn-primary'} onClick={this.handleClose.bind(this)}>Close</button>
+          </Modal.Body>
+          {/* <Modal.Footer>
+            <Button className={'.btn-primary'} onClick={this.handleClose}>Close</Button>
+          </Modal.Footer> */}
+        </Modal>
+        <GridForm>
+          <Row>
+            <Field span={1}>
+              <label>App Name  :</label>
+              <input type="text" value={this.state.ProjectName} onChange={this.OnChangeProject.bind(this)} />
+            </Field>
+          </Row>
+          <Row>
+            <Field span={4}>
+              <label>User Name  :</label>
+              <input type="text" value={this.state.UserId} onChange={this.OnChangeUserID.bind(this)} />
+            </Field>
+          </Row>
+          <Row>
+            <Field span={4}>
+              <label>Password</label>
+              <input type="Password" value={this.state.Password} onChange={this.OnChangePassword.bind(this)} />
+            </Field>
+          </Row>
+          <Row>
+            <Field span={4}>
+              <label>Description</label>
+              <input type="text" value={this.state.description} onChange={this.OnChangeDescription.bind(this)} />
+            </Field>
+          </Row>
+
+          <Row>
+            <Field span={4}>
+
+              <button className={'btn btn-info active'} onClick={this.handleShow}>Submit</button>
+            </Field>
+          </Row>
+        </GridForm>
+
+      </div>
+    );
   }
 }
-
