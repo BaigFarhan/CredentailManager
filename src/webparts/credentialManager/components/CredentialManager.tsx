f4725b73acc1a926aa8a6ffaa375c6346fbf66f6
BaigFarhan <39454407+BaigFarhan@users.noreply.github.com>
2018-05-29 03:30:37 +0400

    Update CredentialManager.tsx

--- src/webparts/credentialManager/components/CredentialManager.tsx
+++ src/webparts/credentialManager/components/CredentialManager.tsx
@@ -7,7 +7,6 @@ import { Button, Modal } from 'react-bootstrap';
 import { FormControl, GridForm, Form, FormGroup, Col, Fieldset, Row, Field } from 'react-gridforms'
 import { PropertyPaneCheckbox } from '@microsoft/sp-webpart-base';
 import Dialog from 'react-bootstrap-dialog';
-import * as jquery from 'jquery';
 import * as pnp from 'sp-pnp-js';
 import {
   SPHttpClient,
@@ -19,7 +18,7 @@ import styless from './CredentialManagerHome.module.scss';
 import { ICredentialManagerHomeProps } from './ICredentialManagerHomeProps';
 import * as ReactDOM from 'react-dom';
 import * as $ from 'jquery';
-import { BootstrapTable, TableHeaderColumn, InsertButton } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, InsertButton, SearchField } from 'react-bootstrap-table';
 import { Data } from './Data'
 import { IData } from './IData'
 var CryptoJS = require("crypto-js");
@@ -41,7 +40,15 @@ export default class CredentialManager extends React.Component<ICredentialManage
       resolve();
     });
   }
+
+  createCustomSearchField = (props) => {
+    return (
+      <SearchField className='my-custom-class' />
+    );
+  }
+
   public state: ICredentialManagerProps;
+
   constructor(props, context) {
 
     super(props);
@@ -54,10 +61,9 @@ export default class CredentialManager extends React.Component<ICredentialManage
       Key: "",
       ShowModal: false,
       spHttpClient: this.props.spHttpClient,
-      SitrUrl: 'https://arabtec.sharepoint.com/sites/dev',
       SucessFullModal: false,
       ErrorModal: false,
-      SiteURL: 'https://arabtec.sharepoint.com/sites/dev',
+      SiteURL: 'https://mirzaa.sharepoint.com/sites/dev',
       Data: this.props.Data,
       ModelShow: false,
       ID: "",
@@ -70,6 +76,9 @@ export default class CredentialManager extends React.Component<ICredentialManage
       copied: false,
       DecryptedPassword: "",
       AddingItem: false,
+      ListName: 'CredentialManager',
+      CurrentUserID: "",
+      DeleteModelShow: false
     }
 
     this.ShowPopupModal = this.ShowPopupModal.bind(this);
@@ -80,7 +89,10 @@ export default class CredentialManager extends React.Component<ICredentialManage
     this.OpenModal = this.OpenModal.bind(this);
     this.CloseModal = this.CloseModal.bind(this);
     this.ShowPassword = this.ShowPassword.bind(this);
-    this.CopyToClipBoard = this.CopyToClipBoard.bind(this);
+    this.OpenDeleteModal = this.OpenDeleteModal.bind(this);
+    this.OpenEditModal = this.OpenEditModal.bind(this);
+
+
     SPComponentLoader.loadScript('https://npmcdn.com/react-bootstrap-table/dist/react-bootstrap-table.min.js');
     SPComponentLoader.loadCss('https://npmcdn.com/react-bootstrap-table/dist/react-bootstrap-table-all.min.css');
     SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css');
@@ -106,7 +118,7 @@ export default class CredentialManager extends React.Component<ICredentialManage
   }
   ShowPopupModal(e) {
     e.preventDefault();
-    debugger;
+
     if (this.state.ProjectName == '') {
       return false;
     }
@@ -143,49 +155,99 @@ export default class CredentialManager extends React.Component<ICredentialManage
   }
 
 
-
   componentDidMount() {
+    // alert('componentDidMount');
+    // this.GetCredentailManagerList();
+  }
+
+  componentWillMount() {
+    // alert('componentWillMount');
     this.GetCredentailManagerList();
+    //this.GetCredentailManagerList();
   }
 
-
-  CopyToClipBoard(e) {
-    debugger;
-    var copyText = document.getElementById("userkey");
-
-    copyText[0].select();
-    document.execCommand('copy');
-  }
   ShowPassword(e) {
-    this.setState({ hidebutton: false }); // yeh copy to clipbard button show kare ga
+    // yeh copy to clipbard button kare ga
     if (this.state.UserEnteredPwd == '') {
       return false;
     }
-    debugger;
+
     var encryptKey = CryptoJS.AES.decrypt(this.state.UserEnteredPwd, this.state.value);
     var plaintext = encryptKey.toString(CryptoJS.enc.Utf8);
     if (plaintext != "") {
       this.setState({ DecryptedPassword: plaintext });
+      this.setState({ hidebutton: false });
     }
     else {
       this.setState({ DecryptedPassword: "Ooops!!!! Wrong Key" });
     }
   }
   OpenModal(e) {
-    debugger;
+
     this.setState({ ModelShow: true });
     this.setState({ ID: e.ID });
     this.setState({ UserEnteredPwd: e.Password });
     this.setState({ key: e.Key });
 
   }
+
+  OpenDeleteModal(e) {
+    this.setState({ DeleteModelShow: true });
+    this.setState({ ID: e.ID });
+  }
+
+  OpenEditModal(e) {
+
+  }
+
+
   onCopy = () => {
     this.setState({ copied: true });
   };
 
-  GetCredentailManagerList() {
+
+  CloseDeleteModal = () => {
+    this.setState({ DeleteModelShow: false });
+  };
+  DeleteRecord(e) {
+
+
+
+
+    let list = pnp.sp.web.lists.getByTitle(this.state.ListName);
+
+    list.items.getById(parseInt(this.state.ID)).delete().then((response) => {
+      this.setState({ DeleteModelShow: false });
+      this.setState({ SucessFullModal: true });
+      this.GetCredentailManagerByCreatedUser();
+
+    }, (error: any): void => {
+      this.setState({ DeleteModelShow: false });
+      this.setState({ ErrorModal: true });
+
+    });
+  }
+
+
+
+  private GetCredentailManagerList() {
+
     var reactHandler = this;
-    var reqUrl = reactHandler.state.SiteURL + "/_api/lists/getbytitle('CredentialManager')/items";
+    var reqUrl = reactHandler.state.SiteURL + "/_api/web/currentUser";
+    $.ajax({
+      url: reqUrl, type: "GET",
+      headers:
+        {
+          "accept": "application/json;odata=verbose"
+        }
+    }).then((response) => {
+      this.setState({ CurrentUserID: response.d.Id });
+      this.GetCredentailManagerByCreatedUser();
+    });
+
+  }
+  GetCredentailManagerByCreatedUser() {
+    var reqUrl = this.state.SiteURL + "/_api/lists/getbytitle('" + this.state.ListName + "')/items?$filter=AuthorId+eq+" + this.state.CurrentUserID;
     $.ajax({
       url: reqUrl, type: "GET",
       headers:
@@ -195,30 +257,8 @@ export default class CredentialManager extends React.Component<ICredentialManage
     }).then((response) => {
       this.setState({ Data: response.d.results });
     });
-
   }
 
-
-  private GetUSerDetails() {
-
-    var reactHandler = this;
-    debugger;
-    var reqUrl = reactHandler.state.SitrUrl + "/_api/lists/getbytitle('CredentialManager')/items";
-    //farhan
-
-    jquery.ajax({
-      url: reqUrl, type: "GET",
-      headers:
-        {
-          "accept": "application/json;odata=verbose"
-        }
-    }).then((response) => {
-      console.log(response.d);
-      debugger;
-
-    });
-
-  }
   SaveData(e) {
 
     var AppName = this.state.ProjectName;
@@ -272,21 +312,34 @@ export default class CredentialManager extends React.Component<ICredentialManage
   }
 
   public render(): React.ReactElement<ICredentialManagerProps> {
-    const buttonFormatter = (cell, row) => {
+    const buttonFormatterDecrypt = (cell, row) => {
       return (
         <div>
-          <button className={styless.btnresponsive} onClick={() => this.OpenModal(row)}>Decrypt</button>
-          <button className={styless.btnresponsive} onClick={() => this.OpenModal(row)}>Delete</button>
-          <button className={styless.btnresponsive} onClick={() => this.OpenModal(row)}>Edit</button>
+          <span onClick={() => this.OpenModal(row)} title="Decrypt Password" style={operationiconcss} className={'glyphicon glyphicon-retweet'}></span>
+        </div>
+      )
+    };
+    const buttonFormatterEdit = (cell, row) => {
+      return (
+        <div>
+          &nbsp;<span onClick={() => this.OpenEditModal(row)} title="Edit Record" style={operationiconcss} className={'glyphicon glyphicon-edit'}></span>
+        </div>
+      )
+    };
+    const buttonFormatterDelete = (cell, row) => {
+      return (
+        <div>
+          &nbsp;  <span onClick={() => this.OpenDeleteModal(row)} title="Delete Record" style={deleteiconcss} className={'glyphicon glyphicon-remove'}></span>
         </div>
       )
     };
 
-
-
-
-
-    const styled = this.state.hidebutton ? { 'display': 'none' } : {};
+    const operationiconcss = { 'color': '#337ab7', 'cursor': 'pointer', 'width': '8%' };
+    const deleteiconcss = { 'color': 'red', 'cursor': 'pointer', 'width': '8%' };
+    const btncopyclipcss = this.state.hidebutton ? { 'display': 'none' } : {};
+    const options = {
+      searchField: this.createCustomSearchField
+    };
 
     return (
       <div className={styles.credentialManager} >
@@ -401,18 +454,29 @@ export default class CredentialManager extends React.Component<ICredentialManage
                 &nbsp;
     <CopyToClipboard text={this.state.DecryptedPassword}
                   onCopy={() => this.setState({ copied: true })}>
-                  <button className={'btn btn-info'} style={styled}>Copy to clipboard</button>
+                  <button className={'btn btn-info'} style={btncopyclipcss}>Copy to clipboard</button>
                 </CopyToClipboard>
               </Modal.Body>
             </Modal>
-            <BootstrapTable
+            <Modal show={this.state.DeleteModelShow} onHide={this.CloseModal}>
+              <Modal.Header >
+                Are You Sure to Delete?
+              </Modal.Header>
+              <Modal.Body>
+                <button className={"btn btn-primary"} onClick={this.DeleteRecord.bind(this)}>Yes</button> &nbsp;
+                <button className={'btn btn-default'} onClick={this.CloseDeleteModal}>No</button>
+              </Modal.Body>
+            </Modal>
+            <BootstrapTable search options={options}
               containerStyle={{ border: '#46b8da 1px solid' }}
               bodyStyle={{ border: '#46b8da 1px solid' }}
               striped={true} hover={true} condensed={true} data={this.state.Data} pagination={true}>
-              <TableHeaderColumn isKey dataField='UserName1'>User Name</TableHeaderColumn>
-              <TableHeaderColumn dataField='AppName'>App Name</TableHeaderColumn>
-              <TableHeaderColumn dataField='Password'>Password</TableHeaderColumn>
-              <TableHeaderColumn dataFormat={buttonFormatter}></TableHeaderColumn>
+              <TableHeaderColumn dataAlign="center" isKey dataField='UserName1'>User Name</TableHeaderColumn>
+              <TableHeaderColumn dataAlign="center" dataField='AppName'>App Name</TableHeaderColumn>
+              <TableHeaderColumn dataAlign="center" dataField='Password'>Password</TableHeaderColumn>
+              <TableHeaderColumn width="8%" dataAlign="center" dataFormat={buttonFormatterDecrypt}></TableHeaderColumn>
+              <TableHeaderColumn width="8%" dataAlign="center" dataFormat={buttonFormatterEdit}></TableHeaderColumn>
+              <TableHeaderColumn width="8%" dataAlign="center" dataFormat={buttonFormatterDelete}></TableHeaderColumn>
             </BootstrapTable>
             <button className={styless.button} onClick={this.AddCredential.bind(this)}>Add Credential</button>
 
